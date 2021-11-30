0cc985f4daf74f535f7611b9485b0188107317f6
mikeisfake <mikescooper@icloud.com>
2021-11-29 23:42:54 -0500

    done

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 function findMinAndRemove(array){
+    min = array[0]
+    mindex = 0;
 
+    for(let i =0; i < array.length; i++) {
+        if(array[i] < min) {
+            min = array[i]
+            mindex = i
+        }
+    }
+
+    array.splice(mindex, 1)
+    return min
 }
 
 function selectionSort(array){
+    let sorted = []
+    let newMin;
 
+    while (array.length != 0) {
+        newMin = findMinAndRemove(array)
+        sorted.push(newMin)
+    }
+    console.log(sorted)
+    return sorted
 }
+let testArr = [1,5,8,7,6,5,4,2,6,32,84,684,2,64,65,84,-4,154,0,6,8,54,2,4]
+
+selectionSort(testArr)
+
+
